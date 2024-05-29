320c9c6b10650a6c65f8a026da18b53ffc644ce1
Jeffrey Schwartz <jefftschwartz@gmail.com>
2024-05-29 08:08:47 -0400

    Open link to snitch repo from cli help
    [closes #62]

--- core/bin/cli.mjs
+++ core/bin/cli.mjs
@@ -1,8 +1,9 @@
 #!/usr/bin/env node
 
+import open from "open"
 import { snitch } from "../index.mjs"
 
 const args = process.argv.slice(2)
 const wantsHelp = ["help", "h", "-h", "--help"].includes(args[0])
-wantsHelp && console.log("For help please see https://github.com/4awpawz/snitch")
+wantsHelp && open("https://github.com/4awpawz/snitch")
 !wantsHelp && await snitch(args)
